42b4051b2be4464a68f1556b31ec777018759ab9
Abdul Hadi <hadi2408466@gmail.com>
2023-12-15 23:31:27 +0500

    Update LaunchTime.jsx

--- src/components/context/LaunchTime.jsx
+++ src/components/context/LaunchTime.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useState } from 'react'
 
 export const LaunchContext = createContext()
-
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export const LaunchContextProvider = ({ children }) => {
   const [showRegister, setShowRegister] = useState(false)
 
